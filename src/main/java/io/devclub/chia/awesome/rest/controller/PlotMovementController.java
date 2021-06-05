89a904f04b587475114913c94c88b571bcc15361
pawelkulpa <pablo.kulpa@gmail.com>
2021-06-05 22:13:27 +0200

    Additional method to plot movement controller.

--- src/main/java/io/devclub/chia/awesome/rest/controller/PlotMovementController.java
+++ src/main/java/io/devclub/chia/awesome/rest/controller/PlotMovementController.java
@@ -1,5 +1,6 @@
 package io.devclub.chia.awesome.rest.controller;
 
+import io.devclub.chia.awesome.rest.request.Plot;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +20,28 @@ public class PlotMovementController {
         return 0;
     }
 
-    @PostMapping(value = "/sendPlot")
-    public void sendPlot(@RequestBody String plotName) {
+    @PostMapping(value = "/acceptPlot")
+    public boolean acceptPlot(@RequestBody Plot plot) {
 
+        // TODO: 05/06/2021
+        String pathToStorePlot = calculateWhereToPutPlot();
+        // TODO: 05/06/2021 store plot on disc;
+        return checkPlotSize(plot);
+    }
+
+    public boolean checkPlotAcceptation() {
+        // TODO: 05/06/2021 check available size on disk to make sure we can store plot
+        return false;
+    }
+
+    public String calculateWhereToPutPlot() {
+        // TODO: 05/06/2021 calculate best Optimal disc to put plot
+        return "";
+    }
+
+    public boolean checkPlotSize(Plot plot) {
+        // TODO: 05/06/2021 check if plot is the same size local and on remote
+        return false;
     }
 
 
